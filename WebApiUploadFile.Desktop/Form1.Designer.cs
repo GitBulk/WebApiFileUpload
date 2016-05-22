97862f1b9e1e34c62f3a834123332ce4a4d1ec15
gitbulk <giacatiensinh@hotmail.com>
2016-05-23 02:46:02 +0700

    update desktop client

--- WebApiUploadFile.Desktop/Form1.Designer.cs
+++ WebApiUploadFile.Desktop/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -38,7 +39,7 @@
             // 
             // btnBrowse
             // 
-            this.btnBrowse.Location = new System.Drawing.Point(174, 29);
+            this.btnBrowse.Location = new System.Drawing.Point(454, 33);
             this.btnBrowse.Name = "btnBrowse";
             this.btnBrowse.Size = new System.Drawing.Size(75, 23);
             this.btnBrowse.TabIndex = 0;
@@ -46,15 +47,26 @@
             this.btnBrowse.UseVisualStyleBackColor = true;
             this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(516, 187);
+            this.textBox1.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(541, 261);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnBrowse);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -62,6 +74,7 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WebApiUploadFile.Desktop/Form1.cs
+++ WebApiUploadFile.Desktop/Form1.cs
@@ -16,69 +16,89 @@ namespace WebApiUploadFile.Desktop
 {
     public partial class Form1 : Form
     {
-        const string RequestUrl = "";
+        const string RequestUrl = "http://localhost:53700/api/fileupload";
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            // Set the file dialog to filter for graphics files.
+            this.openFileDialog1.Filter =
+                "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" +
+                "All files (*.*)|*.*";
+
+            // Allow the user to select multiple images.
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Browse files to upload.";
+        }
+
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            HttpClient client = new HttpClient();
-            try
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
             {
-                foreach (string file in openFileDialog1.FileNames)
+                HttpClient client = new HttpClient();
+                try
                 {
-                    var fileStream = File.Open(file, FileMode.Open);
-                    var fileInfo = new FileInfo(file);
-                    FileUploadResult uploadResult = null;
-                    bool fileUploaded = false;
-                    var content = new MultipartFormDataContent();
-                    content.Add(new StreamContent(fileStream), "\"file\"", string.Format("\"{0}\"", fileInfo.Name));
-                    Task taskUpload = client.PostAsync(RequestUrl, content)
-                        .ContinueWith(task =>
-                        {
-                            if (task.Status == TaskStatus.RanToCompletion)
-                            {
-                                var response = task.Result;
-                                if (response.IsSuccessStatusCode)
-                                {
-                                    uploadResult = response.Content.ReadAsAsync<FileUploadResult>().Result;
-                                    if (uploadResult != null)
-                                    {
-                                        fileUploaded = true;
-                                    }
-                                    foreach (var header in response.Content.Headers)
-                                    {
-                                        Debug.WriteLine("{0}: {1}", header.Key, string.Join(",", header.Value));
-                                    }
-                                }
-                                else
-                                {
-                                    Debug.WriteLine("Status Code: {0} - {1}", response.StatusCode, response.ReasonPhrase);
-                                    Debug.WriteLine("Response Body: {0}", response.Content.ReadAsStringAsync().Result);
-                                }
-                            }
-                            fileStream.Close();
-                        });
-                    taskUpload.Wait();
-                    if (fileUploaded)
+                    foreach (string file in openFileDialog1.FileNames)
                     {
-                        AddMessage(uploadResult.FileName + " with length " + uploadResult.FileLength
-                                                + " has been uploaded at " + uploadResult.LocalFilePath);
+                        var fileStream = File.Open(file, FileMode.Open);
+                        var fileInfo = new FileInfo(file);
+                        FileUploadResult uploadResult = null;
+                        bool fileUploaded = false;
+                        var content = new MultipartFormDataContent();
+                        content.Add(new StreamContent(fileStream), "\"file\"", string.Format("\"{0}\"", fileInfo.Name));
+                        Task taskUpload = client.PostAsync(RequestUrl, content)
+                            .ContinueWith(task =>
+                            {
+                                if (task.Status == TaskStatus.RanToCompletion)
+                                {
+                                    var response = task.Result;
+                                    if (response.IsSuccessStatusCode)
+                                    {
+                                        uploadResult = response.Content.ReadAsAsync<FileUploadResult>().Result;
+                                        if (uploadResult != null)
+                                        {
+                                            fileUploaded = true;
+                                        }
+                                        foreach (var header in response.Content.Headers)
+                                        {
+                                            Debug.WriteLine("{0}: {1}", header.Key, string.Join(",", header.Value));
+                                        }
+                                    }
+                                    else
+                                    {
+                                        Debug.WriteLine("Status Code: {0} - {1}", response.StatusCode, response.ReasonPhrase);
+                                        Debug.WriteLine("Response Body: {0}", response.Content.ReadAsStringAsync().Result);
+                                    }
+                                }
+                                fileStream.Close();
+                            });
+                        taskUpload.Wait();
+                        if (fileUploaded)
+                        {
+                            AddMessage(uploadResult.FileName + " with length " + uploadResult.FileLength
+                                                    + " has been uploaded at " + uploadResult.LocalFilePath);
+                        }
                     }
+                    client.Dispose();
                 }
-                client.Dispose();
-            }
-            catch (Exception ex)
-            {
-                AddMessage(ex.Message);
+                catch (Exception ex)
+                {
+                    AddMessage(ex.Message);
+                }
             }
         }
 
-        private void AddMessage(string v)
+        private void AddMessage(string message)
         {
-            throw new NotImplementedException();
+            textBox1.AppendText(message);
+            textBox1.AppendText(Environment.NewLine);
         }
+
+
     }
 }

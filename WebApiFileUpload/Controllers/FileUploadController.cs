dbb83213eb22e28ccc0898c8db23f4e8a321a4cb
lasthulk <danchithancongab@gmail.com>
2016-05-22 18:24:59 +0700

    create model, controller

---
+++ WebApiFileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WebApiFileUpload.Infrastructure;
+
+namespace WebApiFileUpload.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        [MimeMultipart]
+        public async Task<FileUploadResult> Post()
+        {
+            var uploadPath = HttpContext.Current.Server.MapPath("~/Storage");
+            var multipartFormDataStreamProvider = new UploadMultipartFormProvider(uploadPath);
+            await Request.Content.ReadAsMultipartAsync(multipartFormDataStreamProvider);
+            string localFileName = multipartFormDataStreamProvider.FileData.Select(m => m.LocalFileName).FirstOrDefault();
+
+            return new FileUploadResult
+            {
+                LocalFilePath = localFileName,
+                FileName = Path.GetFileName(localFileName),
+                FileLength = new FileInfo(localFileName).Length
+            };
+        }
+    }
+}
---
+++ WebApiFileUpload/Infrastructure/MimeMultipart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace WebApiFileUpload.Infrastructure
+{
+    public class MimeMultipart : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(new HttpResponseMessage(System.Net.HttpStatusCode.UnsupportedMediaType));
+            }
+            base.OnActionExecuting(actionContext);
+        }
+    }
+}

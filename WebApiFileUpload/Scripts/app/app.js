708832b32c36a46e26145b30172cb9c628c27e0e
gitbulk <giacatiensinh@hotmail.com>
2016-05-22 20:28:30 +0700

    upload files by using angular

---
+++ WebApiFileUpload/Scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+var module = angular.module("myApp", ['ngRoute', 'angularFileUpload']);
+
+module.config(function ($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'Scripts/app/templates/fileUpload.html',
+        controller: 'fileUploadController'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
---
+++ WebApiFileUpload/Scripts/app/controllers/fileUploadController.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+var app = angular.module('myApp', ['angularFileUpload']);
+app.controller('fileUploadController', function ($scope, $http, $timeout, $upload) {
+    $scope.upload = [];
+    $scope.uploadedFiles = [];
+    $scope.startUploading = function ($files) {
+        console.log($files);
+        for (var i = 0; i < $files.length; i++) {
+            var $file = $files[i];
+            (function (index) {
+                $scope.upload[index] = $upload.upload({
+                    url: './api/fileUpload', // web api url
+                    method: 'POST',
+                    file: $file
+                }).progress(function (e) {
+                }).success(function (data, status, headers, config) {
+                    $scope.uploadedFiles.push({
+                        fileName: data.FileName,
+                        filePath: data.LocalFilePath,
+                        fileLength: data.FileLength
+                    });
+                }).error(function (data, status, headers, config) {
+                    console.log(data);
+                });
+            })(i);
+        }
+    }
+});
